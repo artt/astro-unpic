30495e9453c970ff90aa61c284c846ccb4819545
artt <art.tosborvorn@gmail.com>
2024-12-09 23:20:16 +0700

    add vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import { imageService } from "@unpic/astro/service";
+
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
   image: {
     service: imageService(),
   },
+
+  adapter: vercel(),
 });
