1daa043a15fc8276e6686ef63a75ea7486fa39dd
artt <art.tosborvorn@gmail.com>
2024-12-11 23:10:57 +0700

    x

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import { imageService } from "@unpic/astro/service";
+
 // https://astro.build/config
 export default defineConfig({
   image: {
