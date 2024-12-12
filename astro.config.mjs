a3b05ad9419f0aeb9e0734b6a9fc009575aaffba
artt <art.tosborvorn@gmail.com>
2024-12-12 13:28:44 +0700

    x

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import { imageService } from "@unpic/astro/service";
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
   image: {
     service: imageService(),
   },
+  adapter: netlify({ imageCDN: false }),
 });
