7c1016d961e755304711294640cf24a78d7e1213
artt <art.tosborvorn@gmail.com>
2025-01-27 13:16:20 +0700

    x

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ export default defineConfig({
     service: imageService(),
   },
 
-  // adapter: vercel({
-  //   imageService: true,
-  // }),
+  adapter: vercel({
+    imageService: true,
+  }),
 });
